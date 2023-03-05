c311d2717a3b73129249b51c8918b1108a96a992
skokado <terutacchi@gmail.com>
2023-03-05 22:38:21 +0900

    API docs switch if DEBUG

--- django_project/ninja.py
+++ django_project/ninja.py
@@ -1,6 +1,12 @@
+from django.conf import settings
+
 from ninja import NinjaAPI
 
 
 api = NinjaAPI(
     title="My ninja tutorial", version="1.0-beta",
+    docs_url=None,
 )
+
+if settings.DEBUG:
+    api.docs_url = "/docs"
